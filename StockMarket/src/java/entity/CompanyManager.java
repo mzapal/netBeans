5b2191ac014b8947515b205ea7cf241e65d106c5
mzapal <mrowkamz@gmail.com>
2014-08-26 19:43:24 +0200

    Add past quote

--- StockMarket/src/java/entity/CompanyManager.java
+++ StockMarket/src/java/entity/CompanyManager.java
@@ -21,6 +21,11 @@ public class CompanyManager {
         companies.add(new Company("Cat Coal Company", "CCC", CompanyType.ENERGETIC.toString(), 20.0, 11.1, day));
         companies.add(new Company("Cat Com Dot Com", "CCDC", CompanyType.INFORMATIC.toString(), 90.0, 3.4, day));
         companies.add(new Company("Cat coal company", "CCC", CompanyType.ENTERTAINMENT.toString(), 15.0, -11.7, day));
+        
+        Date yesterday = new Date(day.getTime() - 86400000);
+        companies.get(0).getQuotes().add(0, new Quote(18, yesterday, 4.0, companies.get(0)));
+        companies.get(1).getQuotes().add(0, new Quote(87, yesterday, -2.0, companies.get(1)));
+        companies.get(2).getQuotes().add(0, new Quote(17, yesterday, 10.0, companies.get(2)));
     }
 
     public List<Company> getCompanies() {
